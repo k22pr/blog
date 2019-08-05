d9e45dac5b02b240c21ca356e789603c40ba3cfe
ksPr <h2x@pm.me>
2019-08-05 22:08:25 +0900

    init : first

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require("gulp");
+var scss = require("gulp-sass");
+let cleanCSS = require("gulp-clean-css");
+let FileCache = require("gulp-file-cache");
+var fileCache = new FileCache();
+
+gulp.task("scss", function() {
+   return new Promise((resolve) => {
+      gulp
+         .src("./styles/index.scss")
+         .pipe(fileCache.filter())
+         .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
+         .pipe(fileCache.cache())
+         .pipe(gulp.dest("./styles"));
+      resolve();
+   });
+});
+
+gulp.task("scss:component", function() {
+   return new Promise((resolve) => {
+      gulp
+         .src("./styles/component/**/*.scss")
+         .pipe(fileCache.filter())
+         .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
+         .pipe(fileCache.cache())
+         .pipe(gulp.dest("./styles/component/"));
+      resolve();
+   });
+});
+
+gulp.task("watch", function() {
+   gulp.watch("./styles/*.scss", gulp.series("scss"));
+   gulp.watch("./styles/module/*.scss", gulp.series("scss"));
+   gulp.watch("./styles/shared/*.scss", gulp.series("scss"));
+   gulp.watch("./styles/component/**/*.scss", gulp.series("scss:component"));
+});
