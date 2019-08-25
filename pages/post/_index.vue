0cf5110f7a03d2d98ae1d823dd790da5f562e465
ksPr <h2x@pm.me>
2019-08-26 03:23:32 +0900

    feat : 게시글 페이지 axios 호출

--- pages/post/_index.vue
+++ pages/post/_index.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="content-container">post {{ index }}</div>
+   <div class="content-container">post {{ index }}</div>
 </template>
 
 <script lang="ts">
-  import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue } from "vue-property-decorator";
 
-  @Component
-  export default class PostView extends Vue {
-    public index!: number;
-    private created() {
+@Component
+export default class PostView extends Vue {
+   public index!: number;
+   private created() {
       console.log(this.$route.params);
       this.index = this.$route.params.index;
-    }
-  }
+   }
+
+   private async mounted() {
+      console.log(this.$route.params);
+      const id: string = this.$route.params.index;
+      const data: any = this.$http.get(`/posts/${id}`);
+      console.log(data);
+   }
+}
 </script>
 
 <style scoped lang="scss" src="~/styles/component/shared/footer.scss" />
--- plugins/axios.ts
+++ plugins/axios.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import axios from "axios";
 
 Vue.prototype.$http = axios.create({
-   baseURL: "http://localhost/1337",
+   baseURL: "http://localhost:1337",
    timeout: 3000,
 });
 
