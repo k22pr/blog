197fcc7d83c8db6bdd3273504b317484eeba61db
worker <h2x@pm.me>
2019-12-13 02:12:54 +0900

    feat : add newLine.vue

---
+++ components/base/newLine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="line w12" flex fic fjc>
+    <div class="horizon"></div>
+    <i class="fal fa-code-commit"></i>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class ComponentName extends Vue {}
+</script>
+
+<style scoped lang="scss" src="./newLine.scss" />
